42521e1c45d13e88b30444b2616210f12b328f27
Eric Moya Carmona <ericmoyacarmona7@gmail.com>
2025-05-28 13:06:41 +0200

    feat(service): add ShelterService and IShelterService with CRUD operations for shelter entity

---
+++ backend/PetAdoptionAPI/Interfaces/IShelterService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PetAdoptionAPI.Dtos;
+
+namespace PetAdoptionAPI.Interfaces
+{
+    public interface IShelterService
+    {
+        Task<List<ShelterReadDto>> GetAllSheltersAsync();
+        Task<ShelterReadDto> GetShelterByIdAsync(Guid shelterId);
+        Task<ShelterReadDto> CreateShelterAsync(ShelterCreateDto shelterCreateDto);
+        Task<bool> UpdateShelterAsync(Guid shelterId, ShelterUpdateDto shelterUpdateDto);
+        Task<bool> DeleteShelterAsync(Guid shelterId);
+    }
+}
