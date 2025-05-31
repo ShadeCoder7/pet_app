78f63aa98d1742e5301a64669d4fb95c1d76f2d9
Eric Moya Carmona <ericmoyacarmona7@gmail.com>
2025-05-31 18:37:40 +0200

    feat(core): initialize Firebase in main.dart with async main function

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart'; // Import your app's main widget
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // <-- Necesario para inicializar plugins antes de runApp
+  await Firebase.initializeApp();
   runApp(const HopePawsApp());
 }
